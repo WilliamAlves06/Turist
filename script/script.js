a5275698ac997a277c091ab100acf14006b36025
william <you@example.com>
2025-07-07 19:00:27 -0300

    mudando estado do botão assim que o botão for clicado / dar reset no formulario assim que ele for enviado

--- script/script.js
+++ script/script.js
@@ -1,17 +1,24 @@
-const addLoad = () =>{
-  const button = document.querySelector('button');
-  button.innerHTML = '<i class="bi bi-patch-check"></i>';
+const addLoading = () =>{
+  const button = document.getElementById('but');
+  button.innerHTML = 'Cadastrado com sucesso ✅';
 }
 
-addLoad();
+
+const removeLoading = () =>{
+  const button = document.getElementById('but');
+  button.innerHTML = 'Cadastrar novo email';
+}
+
+
 
 const handleSubmit = (Event) =>{
   Event.preventDefault();
+addLoading();
 
-const nome = document.querySelector('input[name=nome').value;
-const sobrenome = document.querySelector('input[name=sobrenome').value;
-const email = document.querySelector('input[name=email').value;
-const gender = document.querySelector('input[name=gender').value;
+const nome = document.querySelector('input[name=nome]').value;
+const sobrenome = document.querySelector('input[name=sobrenome]').value;
+const email = document.querySelector('input[name=email]').value;
+const gender = document.querySelector('input[name=gender]').value;
 
   fetch('https://api.sheetmonkey.io/form/pQ2hcx3mNTcuD3FwjzDB2S',{
     method: 'post',
@@ -20,7 +27,11 @@ const gender = document.querySelector('input[name=gender').value;
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({nome, sobrenome, email, gender}),
+  }).then(() =>{removeLoading()
+     document.getElementById('meuformulario').reset();
   });
+
+ 
 }
 
 document.getElementById('meuformulario').addEventListener('submit', handleSubmit);
